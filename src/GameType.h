417cdf2ecbd11c99c1d92f60c59a9bcfab1ecc5e
kdex <kdex@kdex.de>
2020-07-13 12:40:58 +0200

    Fixed bug where all game types would use pentominoes
    Fixes #1.

--- src/GameType.h
+++ src/GameType.h
@@ -1,10 +1,10 @@
 #pragma once
 namespace Shiro {
     enum class GameType {
-        SIMULATE_QRS,
-        SIMULATE_G1,
-        SIMULATE_G2,
-        SIMULATE_G3,
-        BIG_MODE
+        SIMULATE_QRS = 0x0000,
+        SIMULATE_G1 = 0x0010,
+        SIMULATE_G2 = 0x0020,
+        SIMULATE_G3 = 0x0040,
+        BIG_MODE = 0x8000
     };
 }
