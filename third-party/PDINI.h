3b8d2907a5871402dceb78539e1ba55170683dab
kdex <kdex@kdex.de>
2020-07-24 17:12:59 +0200

    Added `#` as comment token in PDINI

--- third-party/PDINI.h
+++ third-party/PDINI.h
@@ -1,11 +1,11 @@
 /**
  * This is free and unencumbered software released into the public domain.
- * 
+ *
  * Anyone is free to copy, modify, publish, use, compile, sell, or
  * distribute this software, either in source code form or as a compiled
  * binary, for any purpose, commercial or non-commercial, and by any
  * means.
- * 
+ *
  * In jurisdictions that recognize copyright laws, the author or authors
  * of this software dedicate any and all copyright interest in the
  * software to the public domain. We make this dedication for the benefit
@@ -13,7 +13,7 @@
  * successors. We intend this dedication to be an overt act of
  * relinquishment in perpetuity of all present and future rights to this
  * software under copyright law.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
  * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
  * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
@@ -21,7 +21,7 @@
  * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
  * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
  * OTHER DEALINGS IN THE SOFTWARE.
- * 
+ *
  * For more information, please refer to <http://unlicense.org/>
  */
 #pragma once
@@ -82,12 +82,12 @@ namespace PDINI {
 
             // Comments or empty lines:
             // ; example comment
-            const std::regex commentOrEmptyRegex("\\s*(;.*|)");
+            const std::regex commentOrEmptyRegex("\\s*([;#].*|)");
 
             // Sections, which can have comments:
             // [example_section]
             // [example_section] ; comment
-            const std::regex sectionRegex("^\\s*\\[\\s*([_a-zA-Z][_a-zA-Z0-9]*|)\\s*\\]\\s*(;.*|)$");
+            const std::regex sectionRegex("^\\s*\\[\\s*([_a-zA-Z][_a-zA-Z0-9]*|)\\s*\\]\\s*([;#].*|)$");
 
             // Key-value pairs; text after a semicolon is not parsed as a
             // comment:
@@ -287,7 +287,7 @@ namespace PDINI {
          * Converts raw name strings to all uppercase if the INI class was
          * created to match names case insensitively.
          */
-        std::string toName(const std::string& rawName) {
+        std::string toName(const std::string& rawName) const {
             if (caseInsenstive) {
                 std::string convertedName = rawName;
                 for (auto& c : convertedName) {
